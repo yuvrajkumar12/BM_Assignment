4aab0ee2e2be0f23c8687f9b5563757d2c34be83
Yuvraj kumar <115492314+yuvrajkumar12@users.noreply.github.com>
2025-08-24 10:53:03 -0700

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { port: 5173 },
+//   build: {
+//     outDir: 'build'
+//   },
+//   base: '/BM_Assignment/'   // ✅ repo name yaha dalna hai
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
   build: {
-    outDir: 'build'
+    outDir: 'build', // build folder
   },
-  base: '/BM_Assignment/'   // ✅ repo name yaha dalna hai
+  base: '/BM_Assignment/', // <-- repo ka naam
 })
+
